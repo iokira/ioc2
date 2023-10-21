7563a109d8444b0310d23982ed453db752169a3e
iokira <iam.iokiller@gmail.com>
2023-10-21 23:04:55 +0900

    add "add" test

--- src/lexer.rs
+++ src/lexer.rs
@@ -18,4 +18,26 @@ mod tests {
         assert_eq!(vec![Token::IntegerLiteral(1)], lexer(query2));
         assert_eq!(vec![Token::IntegerLiteral(32)], lexer(query3));
     }
+
+    fn add() {
+        let query1 = "1 + 1";
+        let query2 = "12 + 34";
+
+        assert_eq!(
+            vec![
+                Token::IntegerLiteral(1),
+                Token::Add,
+                Token::IntegerLiteral(1)
+            ],
+            lexer(query1)
+        );
+        assert_eq!(
+            vec![
+                Token::IntegerLiteral(12),
+                Token::Add,
+                Token::IntegerLiteral(34)
+            ],
+            lexer(query2)
+        );
+    }
 }

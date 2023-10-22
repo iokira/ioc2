3c40ab3b434583d132b08a97647f5ce53705f257
iokira <iam.iokiller@gmail.com>
2023-10-22 21:11:10 +0900

    add count func

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,8 +1,30 @@
-use crate::token::{ErrorToken, Token};
+use crate::token::{Int, Token, TokenError};
 
-pub fn lexer(s: &str) -> Result<Vec<Token>, ErrorToken> {
+pub fn lexer(_: &str) -> Result<Vec<Token>, TokenError> {
     unimplemented!()
 }
+
+fn count_int(s: &str) -> usize {
+    count(s, |c: char| c.is_digit(10))
+}
+
+fn count_ident(s: &str) -> usize {
+    count(s, |c: char| is_ident_char(c))
+}
+
+fn count(s: &str, pred: fn(char) -> bool) -> usize {
+    fn go(n: usize, s: &str, pred: fn(char) -> bool) -> usize {
+        match s.chars().next() {
+            Some(c) if pred(c) => go(n + 1, &s[1..], pred),
+            _ => n,
+        }
+    }
+    go(0, s, pred)
+}
+
+// fn expect(s: &str, expect: &str) -> bool {
+//     &s[..expect.len()] == expect
+// }
 
 fn is_ident_char(c: char) -> bool {
     c.is_alphabetic() || c == '_'
@@ -11,6 +33,28 @@ fn is_ident_char(c: char) -> bool {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn count_int_test() {
+        let s1 = "123abc";
+        let s2 = "12345";
+        let s3 = "abcde";
+
+        assert_eq!(3, count_int(s1));
+        assert_eq!(5, count_int(s2));
+        assert_eq!(0, count_int(s3));
+    }
+
+    #[test]
+    fn count_ident_test() {
+        let s1 = "abc 123";
+        let s2 = "abc_de 123";
+        let s3 = "12345";
+
+        assert_eq!(3, count_ident(s1));
+        assert_eq!(6, count_ident(s2));
+        assert_eq!(0, count_ident(s3));
+    }
 
     #[test]
     fn one_integer() {

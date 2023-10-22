cbcede87bec2fe8f5faad358a7cbb0146309e506
iokira <iam.iokiller@gmail.com>
2023-10-22 23:49:32 +0900

    add lexer and tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,7 +1,34 @@
 use crate::token::{Ident, Int, Token, TokenError};
 
-pub fn lexer(_: &str) -> Result<Vec<Token>, TokenError> {
-    unimplemented!()
+pub fn lexer(s: &str) -> Result<Vec<Token>, TokenError> {
+    fn go(s: &str, v: Vec<Token>) -> Result<Vec<Token>, TokenError> {
+        if s.len() == 0 {
+            Ok(v)
+        } else if expect_whitespace(s) {
+            go(&s[count_whitespace(s)..], v)
+        } else if expect_int(s) {
+            if let Ok((token, size)) = tokenize_int(s) {
+                go(&s[size..], [v, vec![token]].concat())
+            } else {
+                return Err(TokenError);
+            }
+        } else if expect_ident(s) {
+            if let Ok((token, size)) = tokenize_ident(s) {
+                go(&s[size..], [v, vec![token]].concat())
+            } else {
+                return Err(TokenError);
+            }
+        } else if expect_operators(s) != "" {
+            if let Ok((token, size)) = tokenize_operator(s) {
+                go(&s[size..], [v, vec![token]].concat())
+            } else {
+                return Err(TokenError);
+            }
+        } else {
+            return Ok(v);
+        }
+    }
+    go(s, vec![])
 }
 
 fn tokenize_int(s: &str) -> Result<(Token, usize), TokenError> {
@@ -91,6 +118,10 @@ fn expect_operators(s: &str) -> &'static str {
     }
 
     return "";
+}
+
+fn expect_whitespace(s: &str) -> bool {
+    count_whitespace(s) > 0
 }
 
 fn is_ident_char(c: char) -> bool {
@@ -220,6 +251,15 @@ mod tests {
     }
 
     #[test]
+    fn expect_whitespace_test() {
+        let s1 = " abc";
+        let s2 = "abc ";
+
+        assert_eq!(true, expect_whitespace(s1));
+        assert_eq!(false, expect_whitespace(s2));
+    }
+
+    #[test]
     fn is_ident_char_test() {
         let c1 = 'a';
         let c2 = 'A';
@@ -232,70 +272,54 @@ mod tests {
         assert_eq!(false, is_ident_char(c4));
     }
 
-    // #[test]
-    // fn one_integer() {
-    //     let query1 = "0";
-    //     let query2 = "1";
-    //     let query3 = "32";
-    //
-    //     assert_eq!(Ok(vec![Token::Integer(0)]), lexer(query1));
-    //     assert_eq!(Ok(vec![Token::Integer(1)]), lexer(query2));
-    //     assert_eq!(Ok(vec![Token::Integer(32)]), lexer(query3));
-    // }
+    #[test]
+    fn one_integer() {
+        let query1 = "0";
+        let query2 = "1";
+        let query3 = "32";
 
-    // #[test]
-    // fn add() {
-    //     let query = "1 + 1";
-    //
-    //     assert_eq!(
-    //         vec![
-    //             Token::IntegerLiteral(1),
-    //             Token::Add,
-    //             Token::IntegerLiteral(1)
-    //         ],
-    //         lexer(query)
-    //     );
-    // }
+        assert_eq!(Ok(vec![Token::Integer(0)]), lexer(query1));
+        assert_eq!(Ok(vec![Token::Integer(1)]), lexer(query2));
+        assert_eq!(Ok(vec![Token::Integer(32)]), lexer(query3));
+    }
 
-    // #[test]
-    // fn sub() {
-    //     let query = " 2 - 1";
-    //
-    //     assert_eq!(
-    //         vec![
-    //             Token::IntegerLiteral(2),
-    //             Token::Sub,
-    //             Token::IntegerLiteral(1),
-    //         ],
-    //         lexer(query)
-    //     );
-    // }
+    #[test]
+    fn add() {
+        let query = "1 + 1";
 
-    // #[test]
-    // fn mul() {
-    //     let query = "3 * 5";
-    //
-    //     assert_eq!(
-    //         vec![
-    //             Token::IntegerLiteral(3),
-    //             Token::Mul,
-    //             Token::IntegerLiteral(5),
-    //         ],
-    //         lexer(query)
-    //     );
-    // }
+        assert_eq!(
+            Ok(vec![Token::Integer(1), Token::Add, Token::Integer(1)]),
+            lexer(query)
+        );
+    }
 
-    // #[test]
-    // fn div() {
-    //     let query = "6 / 2";
-    //
-    //     assert_eq!(
-    //         vec![
-    //             Token::IntegerLiteral(6),
-    //             Token::Div,
-    //             Token::IntegerLiteral(2)
-    //         ],
-    //         lexer(query)
-    //     );
-    // }
+    #[test]
+    fn sub() {
+        let query = " 2 - 1";
+
+        assert_eq!(
+            Ok(vec![Token::Integer(2), Token::Sub, Token::Integer(1),]),
+            lexer(query)
+        );
+    }
+
+    #[test]
+    fn mul() {
+        let query = "3 * 5";
+
+        assert_eq!(
+            Ok(vec![Token::Integer(3), Token::Mul, Token::Integer(5),]),
+            lexer(query)
+        );
+    }
+
+    #[test]
+    fn div() {
+        let query = "6 / 2";
+
+        assert_eq!(
+            Ok(vec![Token::Integer(6), Token::Div, Token::Integer(2)]),
+            lexer(query)
+        );
+    }
 }

445d7da2b04839944fe21e1743c1a807a5e81d8b
iokira <iam.iokiller@gmail.com>
2023-10-23 21:22:14 +0900

    add parser test

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,24 @@
-use crate::{
-    token::Token,
-    tree::{Tree, TreeError},
-};
+use crate::{token::Token, tree::*};
 
 pub fn parser(_tokens: Vec<Token>) -> Result<Vec<Tree>, TreeError> {
     unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parser_test() {
+        let tokens = vec![Token::Integer(1), Token::Add, Token::Integer(1)];
+
+        assert_eq!(
+            Ok(vec![Tree::new_tree(
+                NodeKind::Add,
+                Tree::Int(1),
+                Tree::Int(1)
+            )]),
+            parser(tokens)
+        );
+    }
 }

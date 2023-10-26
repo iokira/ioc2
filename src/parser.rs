9b08cec69b6fb427b1c54c1693ed538aab455f96
iokira <iam.iokiller@gmail.com>
2023-10-26 16:01:04 +0900

    add some func

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,46 @@
 use crate::{token::Token, tree::*};
 
-pub fn parser(_tokens: Vec<Token>) -> Result<Vec<Tree>, TreeError> {
+pub fn parser(tokens: Vec<Token>) -> Result<Vec<Tree>, TreeError> {
+    unimplemented!()
+}
+
+fn program(tokens: Vec<Token>) -> Result<Vec<Tree>, TreeError> {
+    unimplemented!()
+}
+
+fn stmt(tokens: Vec<Token>) -> Result<Vec<Tree>, TreeError> {
+    unimplemented!()
+}
+
+fn expr(tokens: Vec<Token>) -> Result<Vec<Tree>, TreeError> {
+    unimplemented!()
+}
+
+fn assign(tokens: Vec<Token>) -> Result<Vec<Tree>, TreeError> {
+    unimplemented!()
+}
+
+fn equality(tokens: Vec<Token>) -> Result<Vec<Tree>, TreeError> {
+    unimplemented!()
+}
+
+fn relational(tokens: Vec<Token>) -> Result<Vec<Tree>, TreeError> {
+    unimplemented!()
+}
+
+fn add(tokens: Vec<Token>) -> Result<Vec<Tree>, TreeError> {
+    unimplemented!()
+}
+
+fn mul(tokens: Vec<Token>) -> Result<Vec<Tree>, TreeError> {
+    unimplemented!()
+}
+
+fn unary(tokens: Vec<Token>) -> Result<Vec<Tree>, TreeError> {
+    unimplemented!()
+}
+
+fn primary(tokens: Vec<Token>) -> Result<Vec<Tree>, TreeError> {
     unimplemented!()
 }
 
